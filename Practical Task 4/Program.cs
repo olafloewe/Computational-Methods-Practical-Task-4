9ad6ff68c6327fed3ca2dde4fd8eb62f1215143a
Olaf Loewe <olafloewe95@gmail.com>
2025-11-26 04:08:05 +0100

    main

---
+++ Practical Task 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_Task_4 {
+    internal class Program {
+        /*
+        public static double[] SystemSolve(double[,] S) {
+            Input:
+                a two-dimensional array of real numbers (the augmented matrix)
+            Output:
+                a one-dimensional array - the solution (or an empty array if there are no or infinitely many solutions)
+            Examples:
+                SystemSolve({{0, 4, 2, -2}, {-2, 3, 1, -7}, {4, 5, 2, 4}})
+                    -> {2, -2, 3}
+                SystemSolve({{1, 3, 5}, {2, 6, 5}})
+                    -> {}
+                SystemSolve({{1, 3, 5}, {3, -2, 4}, {4, -1, 9}, {7, -3, 13}})
+                    -> {2, 1}
+        */
+        public static double[] SystemSolve(double[,] S) {
+            /* Replace this with your code */
+            return new double[] { };
+        }
+
+        public static void Main(string[] args) {
+            /* Feel free to use this method to test your solution. */
+        }
+    }
+}
