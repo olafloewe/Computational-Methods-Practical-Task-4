6285e0ebbfac4095d298c466c4541c92d64f6ecd
Olaf Loewe <olafloewe95@gmail.com>
2025-11-29 02:58:57 +0100

    cleaned up main

--- Practical Task 4/Program.cs
+++ Practical Task 4/Program.cs
@@ -5,6 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Practical_Task_4 {
+
+
+    //  made this..... no idea how to make it work in a 2d array and go back and forth between double and this ¯\_(ツ)_/¯
+    
+    /*
+    // fraction class to increase precision and get rid of rounding errors
+    internal class Fraction{
+        private int numerator;
+        private int denominator;
+        public Fraction(int numerator, int denominator){
+            if (denominator == 0) throw new DivideByZeroException("Denominator cannot be zero.");
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        // single operand plus
+        public static Fraction operator +(Fraction a){
+            return a;
+        }
+
+        // single operand minus
+        public static Fraction operator -(Fraction a){
+            return -a;
+        }
+
+        // double operand plus
+        public static Fraction operator +(Fraction a, Fraction b){
+            return new Fraction((a.numerator * b.denominator) + (b.numerator * a.denominator), a.denominator * b.denominator);
+        }
+
+        // double operand minus
+        public static Fraction operator -(Fraction a, Fraction b){
+            return a + (-b);
+        }
+
+        // double operand multiply
+        public static Fraction operator *(Fraction a, Fraction b){
+            return new Fraction(a.numerator * b.numerator, a.denominator * b.denominator);
+        }
+
+        // double operand division
+        public static Fraction operator /(Fraction a, Fraction b){
+            if (b.numerator == 0) throw new DivideByZeroException("Cannot divide by zero fraction.");
+            return new Fraction(a.numerator * b.denominator, b.numerator * a.denominator);
+        }
+
+        // override default ToString method
+        public override string ToString(){
+            return $"{numerator}/{denominator}";
+        }
+    }
+    */
+
+
     internal class Program {
 
         // Prints the matrix to the console
@@ -15,16 +69,14 @@ namespace Practical_Task_4 {
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine();
         }
 
         // Swaps rows of a matrix in memory reference
         public static void SwapRows(double[,] S, int target, int destination){
             // guard clause
-
-            Console.WriteLine($"width: {S.GetLength(1)} height: {S.GetLength(0)}");
-            if (target >= S.GetLength(0) || destination >= S.GetLength(0) || target < 0 || destination < 0) throw new IndexOutOfRangeException("Row index out of range.");
-            // int width = S.GetLength(1);
             int height = S.GetLength(0);
+            if (target >= height || destination >= height || target < 0 || destination < 0) throw new IndexOutOfRangeException("Row index out of range.");
             double[] tmp = new double[height];
 
             // swap rows
@@ -51,8 +103,8 @@ namespace Practical_Task_4 {
         // Adds two rows of a matrix in memory reference
         public static void AddRows(double[,] S, int target, int addition){
             // guard clause
-            if (target > S.GetLength(0) || addition > S.GetLength(0) || target < 0 || addition < 0) throw new IndexOutOfRangeException("Row index out of range.");
             int width = S.GetLength(1);
+            if (target > width || addition > width || target < 0 || addition < 0) throw new IndexOutOfRangeException("Row index out of range.");
 
             // add rows
             for (int i = 0; i < width; i++){
@@ -60,14 +112,26 @@ namespace Practical_Task_4 {
             }
         }
 
+        // Adds two rows of a matrix in memory reference
+        public static void AddRows(double[,] S, int target, double[] addition){
+            // guard clause
+            int width = S.GetLength(1);
+            if (target > width || target < 0 || addition.Length != width) throw new IndexOutOfRangeException("Row index out of range.");
+
+            // add rows
+            for (int i = 0; i < width; i++){
+                S[target, i] += addition[i]; // add element
+            }
+        }
+
         // sorts the matrix into row echelon form
         public static void RowEchelonForm(double[,] S){
-            int row = 0;
+            // int row = 0;
             int index = 1;
-            do {
+            for(int row = 0; row < S.GetLength(0); row++) {
                 while (S[row, row] == 0) SwapRows(S, row, index++);
                 row++;
-            } while (row < S.GetLength(0));
+            }
         }
 
         /*
@@ -84,96 +148,80 @@ namespace Practical_Task_4 {
                 SystemSolve({{1, 3, 5}, {3, -2, 4}, {4, -1, 9}, {7, -3, 13}})
                     -> {2, 1}
         */
-        public static double[,] SystemSolve(double[,] S){
+        public static double[] SystemSolve(double[,] S){
             int height = S.GetLength(0);
             int width = S.GetLength(1);
 
             // fewer equations than variables => infinitely many solutions
-            if (width + 1 < height) return new double[0,0];
-
-            int limit = width - 1; // last column is constants
+            if (width + 1 < height) return new double[0];
             int row = 0;
 
             do{
                 // row echelon form
                 if (S[row,row] == 0) RowEchelonForm(S);
 
+                try {
+                    ScaleRow(S, row, 1 / S[row, row]); // scale to one
+                }
+                catch (Exception e){
+                    return new double[0]; // no solution
+                }
                 
-                Console.WriteLine($"Pre {row}:");
-                PrintMatrix(S);
-                
-
-                ScaleRow(S, row, 1 / S[row, row]); // scale to one
-
                 // ScaleRow(S, row, -1); // scale to negative coefficient
                 for(int i = 0; i < height; i++){
                     if (i == row) continue; // dont eliminate self
-                    Console.WriteLine($"Eliminating row {i} using row {row} with scale tmp {-S[i, row]}");
+                    if (S[i, row] == 0) continue; // skip zero coefficients
+                    // Console.WriteLine($"Eliminating row {i} using row {row} with scale tmp {-S[i, row]}");
+                    double[] tmpRow = new double[width];
+
                     double tmp = 1 / -S[i, row]; // store scale to revert later
-                    ScaleRow(S, row, -S[i, row]); // coefficient of rows above / below 
-                    AddRows(S, i, row); // eliminate above / below
-                    ScaleRow(S, row, tmp); // scale back
-                    PrintMatrix(S);
+                    // ScaleRow(S, row, -S[i, row]); // coefficient of rows above / below 
+
+                    for (int j = 0; j < width; j++){
+                        tmpRow[j] = S[row, j] * -S[i, row]; // copy and scale row
+                    }
+
+                    AddRows(S, i, tmpRow); // eliminate above / below
+                    // ScaleRow(S, row, tmp); // scale back
+
+                    // PrintMatrix(S);
                 }
                 
-                Console.WriteLine($"Post {row}:");
-                PrintMatrix(S);
-                Console.WriteLine();
-                
-
                 row++;
-            } while (row < limit);
+            } while (row < height);
 
-            // TODO ONE SOLUTION
+            double[] solution = new double[height];
 
-            // TODO NO SOLUTION
+            for (int i = 0; i < height; i++){
+                solution[i] = S[i, width - 1]; // constant column
+            }
 
-            return S;
+            return solution;
+        }
+
+        private static void PrintSolution(double[] solution){
+            // print array solution with formating
+            Console.Write("Solution:\n{ ");
+            for (int i = 0; i < solution.Length; i++){
+                Console.Write($"x{i}={solution[i]}, ");
+            }
+            Console.WriteLine("}");
         }
 
         public static void Main(string[] args) {
-            // height = GetLength(0)
-            // width = GetLength(1)
-
             double[,] augmentedMatrix = new double[,] {
-    {  1,  1,  1,  1, 10 },
-    {  2, -1,  3,  1, 17 },
-    { -3,  4, -1,  2, -2 },
-    {  1,  2,  5, -1, 13 }
-};
+                { 1, 1, 2 },
+                { 2, 2, 4 }
+            };
 
-            Console.WriteLine("Matrix");
+            Console.WriteLine("Matrix: ");
             PrintMatrix(augmentedMatrix);
-            Console.WriteLine();
-            /*
-            Console.WriteLine("Swap");
-            SwapRows(augmentedMatrix, 0, 2);
-            PrintMatrix(augmentedMatrix);
-            Console.WriteLine();
-            */
-            Console.WriteLine("Scale");
-            ScaleRow(augmentedMatrix, 0, 2);
-            PrintMatrix(augmentedMatrix);
-            Console.WriteLine();
-            /*
-            Console.WriteLine("Scale Back");
-            ScaleRow(augmentedMatrix, 0, (double)1/2);
-            PrintMatrix(augmentedMatrix);
-            Console.WriteLine();
-            */
-            /*
-            Console.WriteLine("Add");
-            AddRows(augmentedMatrix, 2, 0);
-            PrintMatrix(augmentedMatrix);
-            Console.WriteLine();
-            */
-            
-            // SystemSolve(augmentedMatrix);
-            PrintMatrix(SystemSolve(augmentedMatrix));
-            Console.WriteLine();
-            
+
+            // solve system
+            PrintSolution(SystemSolve(augmentedMatrix));
+
             // HOLD THE LINE (CMD prompt) !!!
             Console.ReadKey();
-        }
+        }  
     }
 }
